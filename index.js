5ccf01f0fb5cfe363bdc2971e81550be2f3f54fe
Sokmontrey <sythatsokmontrey@gmail.com>
2022-02-12 12:33:01 -0500

    add process.env.port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
