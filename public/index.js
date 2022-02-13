7a7b3342431ce67e126ce0b1be0a60bdd5bd4531
Sokmontrey <sythatsokmontrey@gmail.com>
2022-02-13 12:40:52 -0500

    add recreate func

--- public/index.js
+++ public/index.js
@@ -1,64 +1,107 @@
+import Resize from './resizer.js';
+
 const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', main);
+
+var name = 1;
 
 function main(event){
 	var img = new Image;
 	img.src= URL.createObjectURL(event.target.files[0]);
+
 	img.onload = () => {
 		var base64String;
 		const minChar = document.getElementById('min-char-input').value;
 		const isColor = document.getElementById('is-color-input').checked;
 		if(img.width < img.height){
-			base64String = resizeImg(img, minChar, img.height*minChar/img.width,0);
+			base64String = Resize(img, minChar, img.height*minChar/img.width,0);
 		}else{
-			base64String = resizeImg(img, img.width*minChar/img.height, minChar,0);
+			base64String = Resize(img, img.width*minChar/img.height, minChar,0);
 		}
 		const img_container = document.getElementById('image-container');
 		img_container.innerHTML = `<img src="${base64String}">`;
 
 		getPixels(base64String, isColor, (pixels)=>{
-			createAscii(pixels, isColor);
+			try{
+				const link = createAscii(pixels, isColor);
+				if(link){
+					const button = document.createElement('button');
+					button.innerHTML = 'Download';
+					button.id = 'download-button';
+					button.addEventListener('click', ()=>{
+						link.click();
+					});
+					const container = document.getElementById('save-button-container');
+					container.innerHTML = '';
+					container.appendChild(button);
+				}
+				showReprocess(event);
+			}catch(e){
+				throw new Error(e);
+			}
 		});
 	}
 }
+function showReprocess(event){
+	const container = document.getElementById('recreate-button-container');
+	const button = document.createElement('button');
+	button.innerHTML = 'Recreate';
+	button.id = 'recreate-button';
+	button.addEventListener('click', ()=>{
+		main(event);
+	});
+	container.innerHTML = '';
+	container.appendChild(button);
+}
 
 function createAscii(pixels, isColor){
-	const char_font = 'monospace',
-		char_size = 15,
-		char_width = 12,
-		char_height = 12;
+	try{
+		const char_font = 'monospace',
+			char_size = 15,
+			char_width = 12,
+			char_height = 12;
 
-	const ascii_list = document.getElementById('ascii-list-input').value.split(' ');
-	const canvas = document.getElementById('canvas');
-	canvas.width = pixels[0].length * char_width;
-	canvas.height = pixels.length * char_height;
+		const ascii_list = document.getElementById('ascii-list-input').value.split(' ');
+		const canvas = document.getElementById('canvas');
+		canvas.width = pixels[0].length * char_width;
+		canvas.height = pixels.length * char_height;
 
-	const ctx = canvas.getContext('2d');
-	ctx.fillStyle = '#000';
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = '#000';
+		ctx.fillRect(0,0,canvas.width,canvas.height);
 
-	for(let y=0; y<pixels.length; y++){
-		for(let x=0; x<pixels[0].length; x++){
-			let index,
-				pixel=pixels[y][x];
-			if(isColor){
-				index = Math.floor(
-					(pixel.r+pixel.g+pixel.b)/3 * (ascii_list.length-1)
+		for(let y=0; y<pixels.length; y++){
+			for(let x=0; x<pixels[0].length; x++){
+				let index,
+					pixel=pixels[y][x];
+				if(isColor){
+					index = Math.floor(
+						(pixel.r+pixel.g+pixel.b)/3 * (ascii_list.length-1)
+					);
+					ctx.fillStyle = `rgb(${pixel.r*255},${pixel.g*255},${pixel.b*255})`;
+				}else{
+					index = Math.floor(
+						pixel.g * (ascii_list.length-1)
+					);
+					ctx.fillStyle = `rgb(${pixel.g*255},${pixel.g*255},${pixel.g*255})`;
+				}
+				ctx.font = char_size + 'px ' + char_font;
+				ctx.fillText(
+					ascii_list[index], 
+					x*char_width, 
+					y*char_height + char_size/1.5
 				);
-				ctx.fillStyle = `rgb(${pixel.r*255},${pixel.g*255},${pixel.b*255})`;
-			}else{
-				index = Math.floor(
-					pixel.g * (ascii_list.length-1)
-				);
-				ctx.fillStyle = `rgb(${pixel.g*255},${pixel.g*255},${pixel.g*255})`;
 			}
-			ctx.font = char_size + 'px ' + char_font;
-			ctx.fillText(
-				ascii_list[index], 
-				x*char_width, 
-				y*char_height + char_size/1.5
-			);
 		}
+
+		//save canvas as jpg
+		const link = document.createElement('a');
+		link.download = `ascii_${name}.jpg`;
+		link.href = canvas.toDataURL('image/jpeg', 1.0);
+		name++;
+		return link;
+	}catch(e){
+		throw new Error(e);
 	}
 }
 
@@ -94,79 +137,3 @@ function getPixels(base64String, isColor, callBack){
 	}
 }
 
-function resizeImg(img, maxWidth, maxHeight, degrees) {
-  var imgWidth = img.width,
-    imgHeight = img.height;
-
-  var ratio = 1,
-    ratio1 = 1,
-    ratio2 = 1;
-  ratio1 = maxWidth / imgWidth;
-  ratio2 = maxHeight / imgHeight;
-
-  // Use the smallest ratio that the image best fit into the maxWidth x maxHeight box.
-  if (ratio1 < ratio2) {
-    ratio = ratio1;
-  } else {
-    ratio = ratio2;
-  }
-  var canvas = document.createElement("canvas");
-  var canvasContext = canvas.getContext("2d");
-  var canvasCopy = document.createElement("canvas");
-  var copyContext = canvasCopy.getContext("2d");
-  var canvasCopy2 = document.createElement("canvas");
-  var copyContext2 = canvasCopy2.getContext("2d");
-  canvasCopy.width = imgWidth;
-  canvasCopy.height = imgHeight;
-  copyContext.drawImage(img, 0, 0);
-
-  // init
-  canvasCopy2.width = imgWidth;
-  canvasCopy2.height = imgHeight;
-  copyContext2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
-
-
-  var rounds = 1;
-  var roundRatio = ratio * rounds;
-  for (var i = 1; i <= rounds; i++) {
-
-
-    // tmp
-    canvasCopy.width = imgWidth * roundRatio / i;
-    canvasCopy.height = imgHeight * roundRatio / i;
-
-    copyContext.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvasCopy.width, canvasCopy.height);
-
-    // copy back
-    canvasCopy2.width = imgWidth * roundRatio / i;
-    canvasCopy2.height = imgHeight * roundRatio / i;
-    copyContext2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
-
-  } // end for
-
-  canvas.width = imgWidth * roundRatio / rounds;
-  canvas.height = imgHeight * roundRatio / rounds;
-  canvasContext.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvas.width, canvas.height);
-
-
-  if (degrees == 90 || degrees == 270) {
-    canvas.width = canvasCopy2.height;
-    canvas.height = canvasCopy2.width;
-  } else {
-    canvas.width = canvasCopy2.width;
-    canvas.height = canvasCopy2.height;
-  }
-
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  if (degrees == 90 || degrees == 270) {
-    canvasContext.translate(canvasCopy2.height / 2, canvasCopy2.width / 2);
-  } else {
-    canvasContext.translate(canvasCopy2.width / 2, canvasCopy2.height / 2);
-  }
-  canvasContext.rotate(degrees * Math.PI / 180);
-  canvasContext.drawImage(canvasCopy2, -canvasCopy2.width / 2, -canvasCopy2.height / 2);
-
-
-  var dataURL = canvas.toDataURL();
-  return dataURL;
-}
